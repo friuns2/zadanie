06cfd1c705c7dff537827ac3626d6535825e05eb
Your a <a@example.com>
2022-10-27 18:04:39 +0300

    a

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ fetch('discords.txt')
         const textList = [];
         textList.push(...data.trim().split('\n'));
         // show random text.txt
-        var randomText = textList[Math.floor(Math.random() * textList.length)];
-        document.getElementById('link').innerHTML = randomText;
-        document.getElementById('link').href = randomText;
+        for (let i = 1; i <4; i++) {
+            var randomText = textList[Math.floor(Math.random() * textList.length)];
+            document.getElementById('link'+i).innerHTML = randomText;
+            document.getElementById('link'+i).href = randomText;
+        }
+
     });
 
 fetch('text.txt')
